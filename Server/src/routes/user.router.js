91b566eed8867d2eef5c9ba45a39bd4f0a89d1bb
Rubii <rubenrojomoreno@gmail.com>
2025-05-27 13:29:28 +0200

    fix: carpeta temporal img

--- Server/src/routes/user.router.js
+++ Server/src/routes/user.router.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import multiparty from 'connect-multiparty'
-import { addUserController,updateUserController,getAllUsers,deleteUser } from "../controllers/user.controller.js";
+import { addUserController, updateUserController, getAllUsers, deleteUser } from "../controllers/user.controller.js";
 import { userAuthenticated } from "../middleware/authorization.js";
 
 
-const multipartyMiddleware = multiparty({uploadDir: 'src/uploads/users'})
+const multipartyMiddleware = multiparty({ uploadDir: '/tmp' })
 export const userRouter = Router()
 // userRouter.use(userAuthenticated)
 
-userRouter.post('/register',multipartyMiddleware,addUserController)
-userRouter.patch('/',userAuthenticated,multipartyMiddleware,updateUserController)
-userRouter.get('/all',userAuthenticated,getAllUsers)
+userRouter.post('/register', multipartyMiddleware, addUserController)
+userRouter.patch('/', userAuthenticated, multipartyMiddleware, updateUserController)
+userRouter.get('/all', userAuthenticated, getAllUsers)
 // userRouter.get('/image',userAuthenticated,imageUser)
-userRouter.delete('/',userAuthenticated,deleteUser)
+userRouter.delete('/', userAuthenticated, deleteUser)
